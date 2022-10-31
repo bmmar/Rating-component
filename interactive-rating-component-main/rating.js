020f77e1d9e55397d081be1bcbd73d9d35c86c2b
bmmar <71896547+bmmar@users.noreply.github.com>
2022-10-31 13:21:53 +0000

    Tidying up

--- interactive-rating-component-main/rating.js
+++ interactive-rating-component-main/rating.js
@@ -2,11 +2,7 @@ const thankYou = document.querySelector(".thank-you");
 const submitBtn = document.querySelector("#submit-button")
 const thankText = thankYou.innerHTML;
 const ratingSection = document.querySelector(".rating-section");
-// const btn1 = document.querySelector("button:nth-child(1)")
-// const btn2 = document.querySelector("button:nth-child(2)")
-// const btn3 = document.querySelector("button:nth-child(3)")
-// const btn4 = document.querySelector("button:nth-child(4)")
-// const btn5 = document.querySelector("button:nth-cild(5)")
+
 const ratings = document.getElementsByName('rating');
 let rating = document.querySelector("#rating");
 
@@ -22,18 +18,3 @@ submitBtn.addEventListener('click', () => {
     }
 })
 
-// btn1.addEventListener('click', () => { 
-//     rating.innerText = '1';
-// })
-// btn2.addEventListener('click', () => { 
-//     rating.innerText = "2";
-// })
-// btn3.addEventListener('click', () => { 
-//     rating.innerText = "3";
-// })
-// btn4.addEventListener('click', () => { 
-//     rating.innerText = "4";
-// })
-// btn5.addEventListener('click', () => { 
-//     rating.innerText = "5";
-// })
